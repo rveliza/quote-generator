5a99c3a24fa9b733aed5fbef06c9eb394c28d9ca
Reyner Veliz <36872223+rveliza@users.noreply.github.com>
2022-06-18 22:02:28 -0600

    Fetch data with JS

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// console.log("testing");
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+    // Pick a random quote from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+}
+
+// Get Quotes From API
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        // console.log(apiQuotes);
+        newQuote();
+    } catch (error) {
+        // Catch Error Here
+    }
+}
+
+// On Load
+getQuotes();
